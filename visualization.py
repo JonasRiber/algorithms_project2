b70c8811ecf0e4b313839e81d677374062213b08
grnimrod <nimrod.grandpierre@gmail.com>
2024-02-26 20:21:12 +0100

    Visualization added

---
+++ visualization.py
@@ -0,0 +1,22 @@
+import time
+import matplotlib.pyplot as plt
+from jonas_files.global_align_linear import global_linear
+from jonas_files.global_align_linear import read_phylip_like_matrix
+
+scoring_matrix = read_phylip_like_matrix("./jonas_files/substitution_matrix_phylip.txt")
+
+runtimes = []
+seq1 = "acgtgtcaacgt"
+seq2 = "acgtcgtagcta"
+
+for i in range(30):
+    start_time = time.time()
+    global_linear(seq1 * i, seq2 * i, 5, scoring_matrix)
+    end_time = time.time()
+    runtime = end_time - start_time
+    runtimes.append(runtime)
+
+# Plotting the runtimes
+plt.rcParams['figure.figsize'] = [10, 6]
+plt.plot(range(1, len(runtimes) + 1), runtimes, marker = 'o')
+plt.show()
